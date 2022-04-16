96b72f94dca412bb820edaf07b404251fe571f25
andreeaiulia13 <andreeagherman1320@gmail.com>
2022-04-16 14:58:26 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Notification.destroy_all
+User.destroy_all
+
+User.create(email: 'admin@gmail.com', password: 'admin@1', admin: true)
+User.create(email: 'andreea@gmail.com', password: '123456')
+User.create(email: 'john@gmail.com', password: '123456')
+User.create(email: 'ana@gmail.com', password: '123456')
+
+User.where(admin: false).each do |client|
+  Notification.create(
+    title: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    user_id: client.id
+  )
+
+  Notification.create(
+    title: 'Etiam iaculis ipsum id felis iaculis pharetra. Pellentesque gravida leo nec mollis feugiat.',
+    description: "Fusce tempus, leo vitae tempor porta, nisl tellus sodales ligula, et elementum nisl risus eget odio. Maecenas molestie tellus ultrices orci ultricies accumsan. Curabitur sollicitudin tempor erat ut porta. Phasellus a lobortis est. Integer malesuada congue elit blandit dignissim.",
+    user_id: client.id
+	)
+end
