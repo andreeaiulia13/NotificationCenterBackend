89498752b4ff5001898ab277d5beb149067289a1
andreeaiulia13 <andreeagherman1320@gmail.com>
2022-04-16 15:32:08 +0300

    Improve code

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -3,29 +3,29 @@ require 'rails_helper'
 RSpec.describe Notification, :type => :model do
   let!(:user) { User.create(email: 'email@gmail', password: '123456') }
 
-	context 'with a missing or invalid parameter' do
+  context 'with a missing or invalid parameter' do
     it 'is not valid without a title' do 
-			notification = Notification.new(description: 'description', user_id: user.id)
+      notification = Notification.new(description: 'description', user_id: user.id)
       expect(user).to_not be_valid
-		end
+    end
 
-		it 'is not valid without a description' do 
-			notification = Notification.new(title: 'title', user_id: user.id)
+    it 'is not valid without a description' do 
+      notification = Notification.new(title: 'title', user_id: user.id)
       expect(user).to_not be_valid
-		end
+    end
 
-		it 'is not valid without an user' do 
-			notification = Notification.new(title: 'title', description: 'description')
+    it 'is not valid without an user' do 
+      notification = Notification.new(title: 'title', description: 'description')
       expect(user).to_not be_valid
-		end
-	end
+    end
+  end
 
-	context 'with valid parameters' do
-		it 'puts read false by default' do 
+  context 'with valid parameters' do
+    it 'puts read false by default' do 
       notification = Notification.create(title: 'title', description: 'description', user_id: user.id)
-			
-			expect(notification.read).to eq(false)
-			expect(notification.read_at).to eq(nil)
-		end
-	end
+
+      expect(notification.read).to eq(false)
+      expect(notification.read_at).to eq(nil)
+    end
+  end
 end
